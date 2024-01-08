25c7816aada1a893df414c9bf1027bd954b2b568
Cristian Velazquez <cfvhca@gmail.com>
2024-01-08 08:21:04 -0300

    dashboard ready

---
+++ src/components/Loading.jsx
@@ -0,0 +1,23 @@
+import './Loading.css'
+export const Loading = () => {
+  return (
+    <div className="w-100 d-flex justify-content-center">
+    <div className="loadingio-spinner-spinner-jlzzzpq2428">
+        <div className="ldio-fw0lgqxm5dr">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+         </div>
+    </div>
+    </div>
+  );
+};
